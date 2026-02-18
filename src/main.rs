65e5e8b962d9fe66032a6de8013229b3e50a8fee
Tom König <hi@tomknig.de>
2026-02-18 23:10:15 +0100

    Return a result from the bins main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
-use std::{env, fs, io};
+use anyhow::{Context, Result};
+use std::{env, fs::File, io};
 
 use payments_engine::models::Ledger;
 
 mod adapters;
 mod csv;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn main() -> Result<()> {
+    let mut args = env::args();
 
+    let _program = args.next();
     let transactions_file_path = args
-        .get(1)
-        .expect("Expected a path to a file containing transactions as the first argument");
+        .next()
+        .context("Expected the path to a file containing transactions as the first argument")?;
 
     let mut ledger = Ledger::new();
 
-    let file = fs::File::open(transactions_file_path).expect("Failed to open transactions file");
+    let file = File::open(transactions_file_path)?;
     for result in csv::read(file) {
-        let row = result.expect("Failed to deserialize CSV row");
-
-        let result = ledger.process_transaction(
-            row.try_into()
-                .expect("Failed to convert CSV row to transaction"),
-        );
+        let row = result?;
+        let transaction = row.try_into()?;
+        let result = ledger.process_transaction(transaction);
 
         if let Err(err) = result {
             eprintln!("Error processing transaction: {}", err);
@@ -30,9 +29,7 @@ fn main() {
 
     let stdout = io::stdout();
     let stdout_handle = stdout.lock();
-    let result = csv::write(stdout_handle, ledger.accounts());
+    csv::write(stdout_handle, ledger.accounts())?;
 
-    if let Err(err) = result {
-        eprintln!("Error writing accounts to stdout: {}", err);
-    }
+    Ok(())
 }
